c58afb93dbc93f0a77259c2895c4fb336c4ae967
dimikar <87638763@Dd>
2020-10-21 14:16:51 +0300

    my commit

--- frontend/src/user/Signup.js
+++ frontend/src/user/Signup.js
@@ -1,47 +1,14 @@
-import React, { Component } from "react";
+import React, {Component} from 'react';
+import {signup} from '../auth/index';
 
 class Signup extends Component {
-
-    constructor(){
-        super()
-        this.state = {
-            name: "",
-            email: "",
-            password: "",
-            error: ""
-        }
-    }
-
     render() {
         return (
-                <div className="container">
-             
-                   <h2 className="mt-5" mb-5>Signup</h2>
-
-            <form>
-
-            <div className="form-group">
-                    <lable className="text-muted">Name</lable>
-                    <input type="text" className="form-control"/>
-                </div>
-                <div className="form-group">
-                    <lable className="text-muted">Email</lable>
-                    <input type="email" className="form-control"/>
-                </div>
-                <div className="form-group">
-                    <lable className="text-muted">Username</lable>
-                    <input type="password" className="form-control"/>
-                </div>
-
-                <div className="form-group">
-                    <lable className="text-muted">Password</lable>
-                    <input type="text" className="form-control"/>
-                </div>
-                <button className="btn btn-raised btn-primary">Submit</button>
-            </form>
+            <div className='jumbotron'>
+                <h2>Signup</h2>
             </div>
         );
-    }
-}
+    };
+};
 
 export default Signup;
